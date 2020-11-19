70dd616c3210e8274a1b5204194dfa2a31fa8366
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-11-19 12:54:30 +0530

    Introducting state

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import {TestComponent} from "./components/TestComponent";
 
 class App extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            array: ['apple', 'mango', 'orange', 'banana'],
+            user_name: 'USER 1'
+        };
+    }
+
     componentDidMount() {
         console.log('componentDidMount');
     }
 
     render() {
-        var word = 'tanmay';
-        var array = ['apple', 'mango', 'orange', 'banana'];
+        const word = 'tanmay';
+        const {array} = this.state;
 
         // var arrayToRender = [];
         //
@@ -23,21 +31,24 @@ class App extends Component {
 
         return (
             <div style={{fontSize: '20px'}}>
-                <p>Hi {word}</p>
+                <p>Hi {this.state.user_name}</p>
                 {/*{arrayToRender}*/}
                 <ul>
                     {array.map((word) => {
-                       return <li key={word}>{word}</li>
+                        return <li key={word}>{word}</li>
                     })}
                 </ul>
                 <TestComponent/>
                 <button onClick={() => console.log("Whoaaaa did you just press me ")}>Press Me !</button>
-                <button onClick={this.consoleLog}>Press Me again!</button>
+                <button onClick={this.consoleLog.bind(this)}>Press Me again!</button>
             </div>
         );
     }
 
     consoleLog() {
+        this.setState({
+            user_name: 'User changed'
+        });
         console.log("Whoaaaa did you just press me ... again !");
     }
 
